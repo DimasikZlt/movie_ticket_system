b177b0e3b9854551923cec9963367b32557957b8
DimasikZlt <dimasikzlt@mail.ru>
2020-11-14 15:58:01 +0700

    Test User data_base

--- movie_ticket_system/main.py
+++ movie_ticket_system/main.py
@@ -1,5 +1,7 @@
 from data_base import DataBase
 from role import Role
+from user import User
 
 db = DataBase.connect('../data/movie_ticket_system.sqlite')
 role = Role.create_table(db)
+user = User.create_table(db)
