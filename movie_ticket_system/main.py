3781211ff8094fd7f3cc0420f028a1ceb2c510a3
DimasikZlt <dimasikzlt@mail.ru>
2020-11-14 23:28:35 +0700

    Add creating movie object

--- movie_ticket_system/main.py
+++ movie_ticket_system/main.py
@@ -1,5 +1,7 @@
 from data_base import DataBase
+from movie import Movie
 from user import User
 
 db = DataBase.connect('../data/movie_ticket_system.sqlite')
 user = User.create_table(db)
+movie = Movie.create_table(db)
