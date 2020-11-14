63cf833701974bafbe723ff1606cdaa325cfbb66
DimasikZlt <dimasikzlt@mail.ru>
2020-11-14 21:40:23 +0700

    Add new methods to action with DB

--- movie_ticket_system/user.py
+++ movie_ticket_system/user.py
@@ -1,11 +1,14 @@
 from typing import Any
 
 from data_base import DataBase
+from role import Role
+from tools.field_pair_tuple import FieldPair
 
 
 class User:
-    def __init__(self, data_base: DataBase):
+    def __init__(self, data_base: DataBase, role: Role):
         self.data_base = data_base
+        self.role = role
         self.user_db_fields = (
             'id',
             'first_name',
@@ -13,41 +16,50 @@ class User:
             'login',
         )
 
-    def add(self):
-        pass
+    def add(self, first_name: str, last_name: str, login: str, password: str, role: str):
+        role_id = self.role.get_by_name(role)
+        request = """
+            INSERT INTO user (first_name, last_name, login, password, role_id) VALUES(?, ?, ?, ?, ?)
+        """, (first_name, last_name, login, password, role_id)
+        self.data_base.execute(request)
 
     def get_all(self):
-        pass
+        request = """
+                    SELECT id, first_name, last_name, login
+                    FROM user
+                """
+        return self.data_base.select_all(request)
 
     def get_by_field(self, field_name: str, field_value: Any):
         if field_name in self.user_db_fields:
             request = f"""
-                SELECT *
-                FROM role
+                SELECT id, first_name, last_name, login
+                FROM user
                 WHERE {field_name} = ?
             """, (field_value,)
             return self.data_base.select_one(request)
 
-    def get_by_login(self):
-        pass
+    def remove(self, login):
+        request = """
+            DELETE FROM user
+            WHERE login = ?
+        """, (login,)
+        self.data_base.execute(request)
 
-    def remove(self):
-        pass
-
-    def update(self, field_name: str, field_value: Any, filter_field_name: str,
-               filter_field_value: Any):
-        if (field_name != 'id'
-                and field_name in self.user_db_fields
-                and filter_field_name in self.user_db_fields):
+    def update(self, field_pair: FieldPair, filter_field_pair: FieldPair):
+        if (field_pair.field_name != 'id'
+                and field_pair.field_name in self.user_db_fields
+                and filter_field_pair.field_name in self.user_db_fields):
             request = f"""
                 UPDATE user
-                SET {field_name} = ?
-                WHERE {filter_field_name} = ? 
-            """, (field_value, filter_field_value)
+                SET {field_pair.field_name} = ?
+                WHERE {filter_field_pair.field_name} = ? 
+            """, (field_pair.field_value, filter_field_pair.field_value)
             self.data_base.execute(request)
 
     @classmethod
     def create_table(cls, data_base):
+        role = Role.create_table(data_base)
         request = """
             CREATE TABLE IF NOT EXISTS user (
                 id INTEGER PRIMARY KEY,
@@ -60,4 +72,4 @@ class User:
             );
         """
         data_base.execute(request)
-        return cls(data_base)
+        return cls(data_base, role)
