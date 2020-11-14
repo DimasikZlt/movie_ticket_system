e4f4d755575b592ae54fbf65cdd3626c16a1bd28
DimasikZlt <dimasikzlt@mail.ru>
2020-11-14 21:38:25 +0700

    Optimize DB create by moving Role table to User classmethod

--- movie_ticket_system/main.py
+++ movie_ticket_system/main.py
@@ -1,7 +1,5 @@
 from data_base import DataBase
-from role import Role
 from user import User
 
 db = DataBase.connect('../data/movie_ticket_system.sqlite')
-role = Role.create_table(db)
 user = User.create_table(db)
