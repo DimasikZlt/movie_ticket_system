e18cccd1fe641a6601e32e5e473091679095ef1a
DimasikZlt <dimasikzlt@mail.ru>
2020-11-17 21:50:06 +0700

    Add function has_table for checking exist tables

--- movie_ticket_system/data_base.py
+++ movie_ticket_system/data_base.py
@@ -14,14 +14,30 @@ class DataBase:
         return cls(name, db_connect)
 
     def execute(self, sql_query: Any):
-        self.cur.execute(sql_query)
+        if isinstance(sql_query, tuple):
+            self.cur.execute(*sql_query)
+        else:
+            self.cur.execute(sql_query)
         self.db_connect.commit()
 
     def select_all(self, sql_query: Any):
+        if isinstance(sql_query, tuple):
+            return self.cur.execute(*sql_query).fetchall()
         return self.cur.execute(sql_query).fetchall()
 
     def select_one(self, sql_query: Any):
+        if isinstance(sql_query, tuple):
+            return self.cur.execute(*sql_query).fetchone()
         return self.cur.execute(sql_query).fetchone()
+
+    def has_table(self, table_name: str) -> bool:
+        request = """
+            SELECT COUNT(*) 
+            FROM sqlite_master 
+            WHERE type = 'table' 
+            AND name = ?
+        """, (table_name,)
+        return self.cur.execute(*request).fetchone()[0]
 
     def close(self):
         self.db_connect.close()
