3fe93555a81da399df6be65a2bb96d8222b6a5bf
DimasikZlt <dimasikzlt@mail.ru>
2020-11-24 22:23:02 +0700

    Change class dependencies

--- movie_ticket_system/genre.py
+++ movie_ticket_system/genre.py
@@ -1,48 +1,23 @@
-from __future__ import annotations
-
 from typing import Callable
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from data_base import DataBase
-
-from tools.field_pair_tuple import FieldPair
+from table import Table
 from tools.yaml_loader import load_yaml
 
 
-class Genre:
+class Genre(Table):
     DEFAULT_MOVIES_FILE = '../data/movies.yml'
-
-    def __init__(self, data_base: DataBase):
-        self.data_base = data_base
 
     def add(self, name: str):
         request = """
             INSERT INTO genre(name) VALUES(?)
         """, (name,)
-        self.data_base.execute(request)
+        self.execute(request)
 
     def remove(self, name):
         request = """
             DELETE FROM genre
             WHERE name = ?
         """, (name,)
-        self.data_base.execute(request)
-
-    def get_all(self):
-        request = """
-            SELECT id, name
-            FROM genre
-        """
-        return self.data_base.select_all(request)
-
-    def get_by_field(self, field_pair: FieldPair):
-        request = f"""
-            SELECT id, name
-            FROM genre
-            WHERE {field_pair.field_name} = ?
-        """, (field_pair.field_value,)
-        return self.data_base.select_one(request)
+        self.execute(request)
 
     def load_default_value(self, loader: Callable, movies_file: str):
         genres = set(
@@ -54,15 +29,15 @@ class Genre:
             self.add(genre)
 
     @classmethod
-    def create_table(cls, data_base):
-        genre = cls(data_base)
-        if not data_base.has_table('genre'):
+    def create_table(cls):
+        genre = cls()
+        if not genre.has_table('genre'):
             request = """
                 CREATE TABLE genre (
                     id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL UNIQUE
                 );
             """
-            data_base.execute(request)
+            genre.execute(request)
             genre.load_default_value(load_yaml, Genre.DEFAULT_MOVIES_FILE)
         return genre
--- movie_ticket_system/role.py
+++ movie_ticket_system/role.py
@@ -1,41 +1,19 @@
-from __future__ import annotations
-
 from typing import Callable
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from data_base import DataBase
+from table import Table
 
 from tools.field_pair_tuple import FieldPair
 from tools.yaml_loader import load_yaml
 
 
-class Role:
+class Role(Table):
     DEFAULT_ROLES_FILE = '../data/users.yml'
-
-    def __init__(self, data_base: DataBase):
-        self.data_base = data_base
 
     def add(self, name: str):
         request = """
             INSERT INTO role(name) VALUES(?)
         """, (name,)
-        self.data_base.execute(request)
-
-    def get_all(self):
-        request = """
-            SELECT id, name
-            FROM role
-        """
-        return self.data_base.select_all(request)
-
-    def get_by_field(self, field_pair: FieldPair):
-        request = f"""
-            SELECT id, name
-            FROM role
-            WHERE {field_pair.field_name} = ?
-        """, (field_pair.field_value,)
-        return self.data_base.select_one(request)
+        self.execute(request)
 
     def remove(self, name: str):
         request = """
@@ -50,15 +28,15 @@ class Role:
             self.add(role)
 
     @classmethod
-    def create_table(cls, data_base):
-        role = cls(data_base)
-        if not data_base.has_table('role'):
+    def create_table(cls):
+        role = cls()
+        if not role.has_table('role'):
             request = """
                 CREATE TABLE role (
                     id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL UNIQUE
                 );
             """
-            data_base.execute(request)
+            role.execute(request)
             role.load_default_value(load_yaml, Role.DEFAULT_ROLES_FILE)
         return role
