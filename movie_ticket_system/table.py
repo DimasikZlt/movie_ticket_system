8df1d9a099c9a95d89bc03bbd5d0dcd9b02e38a9
DimasikZlt <dimasikzlt@mail.ru>
2020-11-28 22:56:26 +0700

    Change to abstract class

--- movie_ticket_system/table.py
+++ movie_ticket_system/table.py
@@ -1,14 +1,26 @@
 from data_base import DataBase
 from tools.field_pair_tuple import FieldPair
+from abc import ABCMeta, abstractmethod
 
 
-class Table(DataBase):
+class Table(metaclass=ABCMeta):
+    DB_PATH = '../data/movie_ticket_system.sqlite'
+    DB_CURSOR = DataBase.connect(DB_PATH)
+
+    @abstractmethod
+    def add(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def remove(self):
+        raise NotImplemented
+
     def get_all(self, name: str):
         request = f"""
             SELECT *
             FROM {name}
         """
-        return self.select_all(request)
+        return Table.DB_CURSOR.select_all(request)
 
     def get_by_field(self, name: str, field_pair: FieldPair):
         request = f"""
@@ -16,4 +28,4 @@ class Table(DataBase):
             FROM {name}
             WHERE {field_pair.field_name} = ?
         """, (field_pair.field_value,)
-        return self.select_one(request)
+        return Table.DB_CURSOR.select_one(request)
