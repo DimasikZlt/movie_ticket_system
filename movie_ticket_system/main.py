2d62c59bd88aa58b7bcb7c474112fc75ccd9efce
DimasikZlt <dimasikzlt@mail.ru>
2020-11-29 00:22:49 +0700

    Move creating database to ApplicationDB class

--- movie_ticket_system/main.py
+++ movie_ticket_system/main.py
@@ -1,7 +1,5 @@
+from application_db import ApplicationDB
 from data_base import DataBase
-from movie import Movie
-from user import User
 
-db = DataBase.connect('../data/movie_ticket_system.sqlite')
-user = User.create_table(db)
-movie = Movie.create_table(db)
+
+app_db = ApplicationDB.create_tables()
