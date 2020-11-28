7c66f2f00d712fe6a083666a7b187e4c162ed9c0
DimasikZlt <dimasikzlt@mail.ru>
2020-11-28 23:34:34 +0700

    Rename class and change calling of parent methods

--- movie_ticket_system/genre_table.py
+++ movie_ticket_system/genre_table.py
@@ -1,23 +1,25 @@
 from typing import Callable
+
+from data_base import DataBase
 from table import Table
 from tools.yaml_loader import load_yaml
 
 
-class Genre(Table):
+class GenreTable(Table):
     DEFAULT_MOVIES_FILE = '../data/movies.yml'
 
     def add(self, name: str):
         request = """
             INSERT INTO genre(name) VALUES(?)
         """, (name,)
-        self.execute(request)
+        self.data_base.execute(request)
 
     def remove(self, name):
         request = """
             DELETE FROM genre
             WHERE name = ?
         """, (name,)
-        self.execute(request)
+        self.data_base.execute(request)
 
     def load_default_value(self, loader: Callable, movies_file: str):
         genres = set(
@@ -29,15 +31,15 @@ class Genre(Table):
             self.add(genre)
 
     @classmethod
-    def create_table(cls):
-        genre = cls()
-        if not genre.has_table('genre'):
+    def create_table(cls, data_base: DataBase):
+        genre = cls(data_base)
+        if not data_base.has_table('genre'):
             request = """
                 CREATE TABLE genre (
                     id INTEGER PRIMARY KEY,
                     name TEXT NOT NULL UNIQUE
                 );
             """
-            genre.execute(request)
-            genre.load_default_value(load_yaml, Genre.DEFAULT_MOVIES_FILE)
+            data_base.execute(request)
+            genre.load_default_value(load_yaml, GenreTable.DEFAULT_MOVIES_FILE)
         return genre
