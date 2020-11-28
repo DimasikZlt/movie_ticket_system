fd326f7332e08a33e25f7761359a6e5382944cc8
DimasikZlt <dimasikzlt@mail.ru>
2020-11-29 00:20:14 +0700

    Rename class and change of calling parent methods

--- movie_ticket_system/session_table.py
+++ movie_ticket_system/session_table.py
@@ -8,29 +8,37 @@ from tools.field_pair_tuple import FieldPair
 class SessionTable(Table):
     movie_hall_db_fields = (
         'id',
-        'date_time',
+        'date',
+        'time',
         'movie_hall_id',
-        'movie_title_id'
+        'movie_title_id',
     )
 
-    def add(self, date_time: datetime, movie_hall_name: int, movie_title: str):
+    def add(self, date_time: datetime, movie_hall_name: str, movie_title: str):
         movie_title_id, _ = super().get_by_field('movie_title', FieldPair('name', movie_title))
         movie_hall_id, _ = super().get_by_field('movie_hall', FieldPair('name', movie_hall_name))
+        date, time = date_time.strftime('%d.%m.%Y'), date_time.strftime('%H:%M')
         request = """
-            INSERT INTO session(date_time, movie_hall_id, movie_title_id) VALUES(?, ?, ?)
-        """, (date_time.strftime('%d.%m.%Y %H:%M'), movie_hall_id, movie_title_id)
+            INSERT INTO session(date, time, movie_hall_id, movie_title_id) VALUES(?, ?, ?, ?)
+        """, (date, time, movie_hall_id, movie_title_id)
         self.data_base.execute(request)
 
-    def remove(self, date_time: datetime, ):
+    def remove(self, date_time: datetime, movie_hall_name: str, movie_title: str):
+        movie_title_id, _ = super().get_by_field('movie_title', FieldPair('name', movie_title))
+        movie_hall_id, _ = super().get_by_field('movie_hall', FieldPair('name', movie_hall_name))
+        date, time = date_time.strftime('%d.%m.%Y'), date_time.strftime('%H:%M')
         request = """
             DELETE FROM session
-            WHERE date_time = ?
-        """, (date_time.strftime('%d.%m.%Y %H:%M'),)
+            WHERE date = ?
+            AND time = ?
+            AND movie_title_id = ?
+            AND movie_hall_id = ?
+        """, (date, time, movie_title_id, movie_hall_id)
         self.data_base.execute(request)
 
     def get_all(self, table_name: str):
         request = f"""
-            SELECT session.id, session.date_time, movie_hall.name, movie.title
+            SELECT session.id, session.date, session.time, movie_hall.name, movie.title
             FROM {table_name}
             INNER JOIN movie_hall on movie_hall.id = session.movie_hall_id
             INNER JOIN movie on movie.id = session.movie_title_id
@@ -40,7 +48,7 @@ class SessionTable(Table):
     def get_by_field(self, table_name: str, field_pair: FieldPair):
         if field_pair.field_name in self.movie_hall_db_fields:
             request = f"""
-                SELECT session.id, session.date_time, movie_hall.name, movie.title
+                SELECT session.id, session.date, session.time, movie_hall.name, movie.title
                 FROM {table_name}
                 INNER JOIN movie_hall on movie_hall.id = session.movie_hall_id
                 INNER JOIN movie on movie.id = session.movie_title_id
@@ -55,9 +63,10 @@ class SessionTable(Table):
             request = """
                 CREATE TABLE session (
                     id INTEGER PRIMARY KEY,
-                    date_time TEXT NOT NULL,
-                    movie_hall_id INTEGER NOT NULL UNIQUE REFERENCES movie_hall(id),
-                    movie_title_id INTEGER NOT NULL UNIQUE REFERENCES movie(id)
+                    date TEXT NOT NULL,
+                    time TEXT NOT NULL,
+                    movie_hall_id INTEGER REFERENCES movie_hall(id),
+                    movie_title_id INTEGER REFERENCES movie(id)
                 );
             """
             data_base.execute(request)
