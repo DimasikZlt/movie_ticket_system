9945037f980dc25932e909fd94736755fd545b30
DimasikZlt <dimasikzlt@mail.ru>
2020-11-29 00:06:08 +0700

    Rename class and change of calling parent methods

--- movie_ticket_system/session_table.py
+++ movie_ticket_system/session_table.py
@@ -1,31 +1,21 @@
-from __future__ import annotations
-
 from datetime import datetime
-from typing import TYPE_CHECKING
 
+from data_base import DataBase
+from table import Table
 from tools.field_pair_tuple import FieldPair
 
-if TYPE_CHECKING:
-    from data_base import DataBase
-    from movie import Movie
-    from movie_hall import MovieHall
 
-
-class Session:
-    def __init__(self, data_base: DataBase, movie: Movie, movie_hall: MovieHall):
-        self.data_base = data_base
-        self.movie = movie
-        self.movie_hall = movie_hall
-        self.movie_hall_db_fields = (
-            'id',
-            'date_time',
-            'movie_hall_id',
-            'movie_title_id'
-        )
+class SessionTable(Table):
+    movie_hall_db_fields = (
+        'id',
+        'date_time',
+        'movie_hall_id',
+        'movie_title_id'
+    )
 
     def add(self, date_time: datetime, movie_hall_name: int, movie_title: str):
-        movie_title_id = self.movie.get_by_field(FieldPair('title', movie_title))[0]
-        movie_hall_id = self.movie_hall.get_by_field(FieldPair('name', movie_hall_name))
+        movie_title_id, _ = super().get_by_field('movie_title', FieldPair('name', movie_title))
+        movie_hall_id, _ = super().get_by_field('movie_hall', FieldPair('name', movie_hall_name))
         request = """
             INSERT INTO session(date_time, movie_hall_id, movie_title_id) VALUES(?, ?, ?)
         """, (date_time.strftime('%d.%m.%Y %H:%M'), movie_hall_id, movie_title_id)
@@ -38,20 +28,20 @@ class Session:
         """, (date_time.strftime('%d.%m.%Y %H:%M'),)
         self.data_base.execute(request)
 
-    def get_all(self):
-        request = """
+    def get_all(self, table_name: str):
+        request = f"""
             SELECT session.id, session.date_time, movie_hall.name, movie.title
-            FROM session
+            FROM {table_name}
             INNER JOIN movie_hall on movie_hall.id = session.movie_hall_id
             INNER JOIN movie on movie.id = session.movie_title_id
         """
         return self.data_base.select_all(request)
 
-    def get_by_field(self, field_pair: FieldPair):
+    def get_by_field(self, table_name: str, field_pair: FieldPair):
         if field_pair.field_name in self.movie_hall_db_fields:
             request = f"""
                 SELECT session.id, session.date_time, movie_hall.name, movie.title
-                FROM session
+                FROM {table_name}
                 INNER JOIN movie_hall on movie_hall.id = session.movie_hall_id
                 INNER JOIN movie on movie.id = session.movie_title_id
                 WHERE session.{field_pair.field_name} = ?
@@ -59,8 +49,8 @@ class Session:
             return self.data_base.select_one(request)
 
     @classmethod
-    def create_table(cls, data_base: DataBase, movie: Movie, movie_hall: MovieHall):
-        session = cls(data_base, movie, movie_hall)
+    def create_table(cls, data_base: DataBase):
+        session = cls(data_base)
         if not data_base.has_table('session'):
             request = """
                 CREATE TABLE session (
