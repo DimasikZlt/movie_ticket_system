4f1155166ed43d7d506b0f65aeb12e2440beb122
DimasikZlt <dimasikzlt@mail.ru>
2020-11-29 15:51:01 +0700

    Add default database value and abstract methods

--- movie_ticket_system/table.py
+++ movie_ticket_system/table.py
@@ -4,9 +4,19 @@ from abc import ABCMeta, abstractmethod
 
 
 class Table(metaclass=ABCMeta):
+    DB_PATH = '../data/movie_ticket_system.sqlite'
+    DATABASE = DataBase.connect(DB_PATH)
 
-    def __init__(self, db: DataBase):
-        self.data_base = db
+    def __init__(self, db: DataBase = None):
+        self.data_base = db or Table.DATABASE
+
+    @abstractmethod
+    def add(self, *args, **kwargs):
+        raise NotImplemented
+
+    @abstractmethod
+    def remove(self, *args, **kwargs):
+        raise NotImplemented
 
     def get_all(self, name: str):
         request = f"""
