630bbd6298a56f26483c9f83e112cf82a756300f
DimasikZlt <dimasikzlt@mail.ru>
2020-11-29 17:59:50 +0700

    Create new tables

--- movie_ticket_system/application_db.py
+++ movie_ticket_system/application_db.py
@@ -2,7 +2,10 @@ from genre_table import GenreTable
 from movie_hall_table import MovieHallTable
 from movie_table import MovieTable
 from role_table import RoleTable
+from row_table import RowTable
+from seat_table import SeatTable
 from session_table import SessionTable
+from ticket_table import TicketTable
 from user_table import UserTable
 
 
@@ -14,3 +17,6 @@ class ApplicationDB:
         self.movie = MovieTable.create_table()
         self.movie_hall = MovieHallTable.create_table()
         self.session = SessionTable.create_table()
+        self.row = RowTable.create_table()
+        self.seat = SeatTable.create_table()
+        self.ticket = TicketTable.create_table()
