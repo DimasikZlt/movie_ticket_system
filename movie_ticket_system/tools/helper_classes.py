139b7dbe5e5596fd4ce5d1847d7a568d5e5a359d
DimasikZlt <dimasikzlt@mail.ru>
2020-12-10 23:06:32 +0700

    Add new Seat class

--- movie_ticket_system/tools/helper_classes.py
+++ movie_ticket_system/tools/helper_classes.py
@@ -15,3 +15,11 @@ class Session(NamedTuple):
     movie_hall: str
     movie_title_id: int
     movie_title: str
+
+
+class Seat(NamedTuple):
+    seat_id: int
+    row_id: int
+    row_number: int
+    seat_number: int
+    movie_hall_id: int
