171d63c87a3c289fce469a296e50f17c9d84cced
DimasikZlt <dimasikzlt@mail.ru>
2020-12-10 23:09:18 +0700

    Move tabWidget object to main_window function

--- movie_ticket_system/time_table_panel.py
+++ movie_ticket_system/time_table_panel.py
@@ -7,9 +7,6 @@ from tools.helper_classes import Session
 
 
 def make_time_table_panel(parent: QtWidgets):
-    parent.tab_session = QtWidgets.QWidget()
-    parent.tab_session.setEnabled(True)
-    parent.tab_session.setObjectName("tab_session")
     parent.horizontalLayout_3 = QtWidgets.QHBoxLayout(parent.tab_session)
     parent.horizontalLayout_3.setObjectName("horizontalLayout_3")
     parent.leftVerticalLayout = QtWidgets.QVBoxLayout()
@@ -32,4 +29,3 @@ def make_time_table_panel(parent: QtWidgets):
     parent.sessionHorizontalLayout.addLayout(parent.leftVerticalLayout)
     parent.sessionHorizontalLayout.setStretch(0, 30)
     parent.horizontalLayout_3.addLayout(parent.sessionHorizontalLayout)
-    parent.tabWidget.addTab(parent.tab_session, "Sessions")
