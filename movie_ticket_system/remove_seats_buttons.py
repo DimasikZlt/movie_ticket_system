60197a486122b0bc7f3081087e809f6bbe624194
DimasikZlt <dimasikzlt@mail.ru>
2020-12-14 08:34:01 +0700

    Remove session and seats buttons

--- movie_ticket_system/remove_seats_buttons.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QWidget
-
-
-def remove_seats_buttons(view: QWidget):
-    while view.gridLayout.count():
-        child = view.gridLayout.takeAt(0)
-        if child.widget():
-            child.widget().deleteLater()
