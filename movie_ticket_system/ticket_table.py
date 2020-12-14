6a04b528003ae5e282be91137654816239ae4ea8
DimasikZlt <dimasikzlt@mail.ru>
2020-12-14 23:24:13 +0700

    Add three requests for adding, removing and getting booked seats

--- movie_ticket_system/ticket_table.py
+++ movie_ticket_system/ticket_table.py
@@ -1,12 +1,34 @@
 from table import Table
+from tools.helper_classes import Seat, Session
 
 
 class TicketTable(Table):
-    def add(self):
-        pass
+    def add(self, seat: Seat):
+        request = """
+            INSERT INTO ticket(session_id, row_id, seat_id) VALUES(?, ?, ?)
+        """
+        self.data_base.execute(request, (seat.session_id, seat.row_id, seat.seat_id))
 
-    def remove(self):
-        pass
+    def remove(self, seat: Seat):
+        request = """
+            DELETE FROM ticket
+            WHERE session_id = ?
+            AND row_id = ?
+            AND seat_id = ?
+        """
+        self.data_base.execute(request, (seat.session_id, seat.row_id, seat.seat_id))
+
+    def get_booked_seats(self, session: Session):
+        request = """
+            SELECT session.movie_hall_id, ticket.row_id, row.row_number, ticket.seat_id, 
+              seat.seat_number, session.id
+            FROM ticket
+            INNER JOIN session on ticket.session_id = session.id 
+            INNER JOIN row on ticket.row_id = row.id
+            INNER JOIN seat on ticket.seat_id = seat.id
+            WHERE ticket.session_id = ?
+        """
+        return self.data_base.select_all(request, (session.session_id,))
 
     @classmethod
     def create_table(cls):
