e259d59a970cda4e49e51e633a370385bdae7a59
DimasikZlt <dimasikzlt@mail.ru>
2020-12-14 08:32:03 +0700

    Fix calling GUI items

--- movie_ticket_system/time_table_panel.py
+++ movie_ticket_system/time_table_panel.py
@@ -1,31 +1,31 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QWidget
 
 from session_push_button import SessionPushButton
 from tools.helper_classes import Session
 
 
-def make_time_table_panel(parent: QtWidgets):
-    parent.horizontalLayout_3 = QtWidgets.QHBoxLayout(parent.tab_session)
-    parent.horizontalLayout_3.setObjectName("horizontalLayout_3")
-    parent.leftVerticalLayout = QtWidgets.QVBoxLayout()
-    parent.leftVerticalLayout.setObjectName("leftVerticalLayout")
-    parent.scrollArea = QtWidgets.QScrollArea(parent.tab_session)
-    parent.scrollArea.setWidgetResizable(True)
-    parent.scrollArea.setObjectName("scrollArea")
-    parent.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-    parent.scrollAreaWidgetContents = QtWidgets.QWidget()
-    parent.btn_layoutVerticalLayout = QtWidgets.QVBoxLayout(parent.scrollAreaWidgetContents)
-    parent.btn_layoutVerticalLayout.setObjectName("btn_layoutVerticalLayout")
+def make_time_table_panel(parent: QWidget):
+
+    leftVerticalLayout = QtWidgets.QVBoxLayout()
+    leftVerticalLayout.setObjectName("leftVerticalLayout")
+    scrollArea = QtWidgets.QScrollArea(parent.tab_session)
+    scrollArea.setWidgetResizable(True)
+    scrollArea.setObjectName("scrollArea")
+    scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+    scrollAreaWidgetContents = QtWidgets.QWidget()
+    btn_layoutVerticalLayout = QtWidgets.QVBoxLayout(scrollAreaWidgetContents)
+    btn_layoutVerticalLayout.setObjectName("btn_layoutVerticalLayout")
     sessions = parent.app_db.session.get_sessions_by_date(datetime.now().date())
 
     for session in sessions:
-        btn = SessionPushButton(Session(*session), parent.scrollAreaWidgetContents)
-        parent.btn_layoutVerticalLayout.addWidget(btn)
+        btn = SessionPushButton(Session(*session), scrollAreaWidgetContents)
+        btn_layoutVerticalLayout.addWidget(btn)
 
-    parent.scrollArea.setWidget(parent.scrollAreaWidgetContents)
-    parent.leftVerticalLayout.addWidget(parent.scrollArea)
-    parent.sessionHorizontalLayout.addLayout(parent.leftVerticalLayout)
-    parent.sessionHorizontalLayout.setStretch(0, 30)
-    parent.horizontalLayout_3.addLayout(parent.sessionHorizontalLayout)
+    scrollArea.setWidget(scrollAreaWidgetContents)
+    leftVerticalLayout.addWidget(scrollArea)
+    parent.sessionHorizontalLayout.addLayout(leftVerticalLayout)
+    # parent.sessionHorizontalLayout.setStretch(0, 30)
+    # parent.horizontalLayout_3.addLayout(parent.sessionHorizontalLayout, 30)
