54e4964f498d07635dd093f49e0fe246716bad63
DimasikZlt <dimasikzlt@mail.ru>
2020-12-16 12:06:08 +0700

    Add some comments

--- movie_ticket_system/table.py
+++ movie_ticket_system/table.py
@@ -1,9 +1,15 @@
+from typing import List, Tuple, Any
+
 from data_base import DataBase
 from tools.helper_classes import FieldPair
 from abc import ABCMeta, abstractmethod
 
 
 class Table(metaclass=ABCMeta):
+    """
+    Abstract class describes a table of database
+    Contains some basic methods
+    """
     DB_PATH = '../data/movie_ticket_system.sqlite'
     DATABASE = DataBase.connect(DB_PATH)
 
@@ -18,14 +24,14 @@ class Table(metaclass=ABCMeta):
     def remove(self, *args, **kwargs):
         raise NotImplemented
 
-    def get_all(self, name: str):
+    def get_all(self, name: str) -> List[Tuple[Any]]:
         request = f"""
             SELECT *
             FROM {name}
         """
         return self.data_base.select_all(request)
 
-    def get_by_field(self, name: str, field_pair: FieldPair):
+    def get_by_field(self, name: str, field_pair: FieldPair) -> Tuple[Any]:
         request = f"""
             SELECT *
             FROM {name}
